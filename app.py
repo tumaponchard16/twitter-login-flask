b93cfcb8e2a7977e311220ada45629483e4a0f09
Richard <tumaponchard16@gmail.com>
2018-09-24 15:46:55 +0800

    twitter authorization

--- app.py
+++ app.py
@@ -1,21 +1,44 @@
-from flask import Flask, render_template, session, redirect
-from twitter_utils import get_request_token, get_oauth_verifier_url
+from flask import Flask, render_template, session, redirect, request
+from twitter_utils import get_request_token, get_oauth_verifier_url, get_access_token
+from user import User
+from database import Database
 
 app = Flask(__name__)
 app.secret_key = "1234"
 
+# Initialize database
+Database.initialise(user='postgres', password='16redroses', host='localhost', database='learning')
 
+
+# get Homepage
 @app.route('/')
 def homepage():
     return render_template('home.html')
 
 
+# Twitter login endpoint
 @app.route('/login/twitter')
 def twitter_login():
     request_token = get_request_token()
     session['request_token'] = request_token
 
     return redirect(get_oauth_verifier_url(request_token))
+
+
+# twitter authorization
+@app.route('/auth/twitter')
+def twitter_auth():
+    oauth_verifier = request.args.get('oauth_verifier')
+    access_token = get_access_token(session['request_token'], oauth_verifier)
+
+    user = User.load_from_db_by_screen_name(access_token['screen_name'])
+    if not user:
+        user = User(access_token['screen_name'], access_token['oauth_token'], access_token['oauth_token_secret'], None)
+        user.save_to_db()
+
+    session['screen_name'] = user.screen_name
+
+    return user.screen_name
 
 
 app.run(port=4995)
--- user.py
+++ user.py
@@ -5,36 +5,32 @@ import json
 
 
 class User:
-    def __init__(self, email, first_name, last_name, oauth_token, oauth_token_secret, id):
-        self.email = email
-        self.first_name = first_name
-        self.last_name = last_name
+    def __init__(self, screen_name, oauth_token, oauth_token_secret, id):
+        self.screen_name = screen_name
         self.oauth_token = oauth_token
         self.oauth_token_secret = oauth_token_secret
         self.id = id
 
     def __repr__(self):
-        return "<User {}>".format(self.email)
+        return "<User {}>".format(self.screen_name)
 
     # save data to database
     def save_to_db(self):
         with CursorFromConnectionFromPool() as cursor:
             cursor.execute(
-                "INSERT INTO users (email, first_name, last_name, oauth_token, oauth_token_secret) VALUES (%s, %s, %s, %s, %s)",
-                (self.email, self.first_name, self.last_name, self.oauth_token, self.oauth_token_secret))
+                "INSERT INTO users (screen_name, oauth_token, oauth_token_secret) VALUES (%s, %s, %s)",
+                (self.screen_name, self.oauth_token, self.oauth_token_secret))
 
     # Load data from database
     @classmethod
-    def load_from_db_by_email(cls, email):
+    def load_from_db_by_screen_name(cls, screen_name):
         with CursorFromConnectionFromPool() as cursor:
-            cursor.execute('SELECT * FROM users WHERE email=%s', (email,))
+            cursor.execute('SELECT * FROM users WHERE screen_name=%s', (screen_name,))
             user_data = cursor.fetchone()
             if user_data:
-                return cls(email=user_data[1],
-                           first_name=user_data[2],
-                           last_name=user_data[3],
-                           oauth_token=user_data[4],
-                           oauth_token_secret=user_data[5],
+                return cls(screen_name=user_data[1],
+                           oauth_token=user_data[2],
+                           oauth_token_secret=user_data[3],
                            id=user_data[0])
 
     def twitter_request(self, uri, verb='GET'):
